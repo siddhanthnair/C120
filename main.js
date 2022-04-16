487373af11da6623283d03f6b8702f038a15e98d
siddhanthnair <72081663+siddhanthnair@users.noreply.github.com>
2022-04-16 11:44:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw(){
+  image(video,0, 0, 300, 300);
+}
+
+
+
